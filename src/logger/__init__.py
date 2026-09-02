ebc4c4a1fba729269e76e59452388d1bf3d5f499
dhruvin kathiriya <dhruvinkathiriya1366@gmail.com>
2026-09-02 18:09:25 +0530

    add exception

--- src/logger/__init__.py
+++ src/logger/__init__.py
@@ -1,20 +1,53 @@
-import logging 
-from pathlib import Path 
+import logging
+from pathlib import Path
 from datetime import datetime
- 
-ROOT_DIR=Path(__file__).resolve().parents[2]
-LOG_DIR=ROOT_DIR/"logs"
-LOG_DIR.mkdir(parents=True,exist_ok=True)
 
-LOG_FILE=LOG_DIR/f"{datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.log"
 
-logging.basicConfig( filename=LOG_FILE, 
-                    level=logging.INFO, 
-                    format="[%(asctime)s] [%(filename)s:%(lineno)d] %(levelname)s - %(message)s",
-                    datefmt="%d-%m-%Y %H:%M:%S"
-                    )
-logger=logging.getLogger(__name__)
-if __name__ == "__main__": 
-    logger.info("Logger has been initialized successfully.")
-    logger.info(f"Log file created at: {LOG_FILE}") 
-    print(f"Log file created: {LOG_FILE}")
+# Project root
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+
+# Log directory
+LOG_DIR = PROJECT_ROOT / "logs"
+LOG_DIR.mkdir(parents=True, exist_ok=True)
+
+
+# Log file name
+current_time = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
+
+LOG_FILE = LOG_DIR / f"{current_time}.log"
+
+
+# Create logger
+logger = logging.getLogger("customer_churn_logger")
+
+logger.setLevel(logging.INFO)
+
+# Prevent duplicate handlers
+logger.propagate = False
+
+
+# File handler
+file_handler = logging.FileHandler(
+    LOG_FILE,
+    encoding="utf-8"
+)
+
+console_handler=logging.StreamHandler()
+file_handler.setLevel(logging.INFO)
+
+
+# Log format
+formatter = logging.Formatter(
+    "[%(asctime)s] "
+    "%(levelname)s "
+    "[%(filename)s:%(lineno)d] "
+    "%(funcName)s() - "
+    "%(message)s",
+    datefmt="%d-%m-%Y %H:%M:%S"
+)
+
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
