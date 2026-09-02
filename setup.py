1e4e52e283ae3aa5339a18e523c405080db48634
dhruvin kathiriya <dhruvinkathiriya1366@gmail.com>
2026-09-02 18:08:36 +0530

    updaterequirement

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="src",
+    version="0.1.0",
+    author="Dhruv",
+    description="Customer Churn Prediction MLOps Project",
+    packages=find_packages()
+)
